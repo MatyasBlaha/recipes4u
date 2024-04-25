05d99518156cbca6071806e9b90b22715aa4651f
MatyasBlaha <matyasblaha1@gmail.com>
2024-04-25 23:40:21 +0200

    add clock icon in RecipeDetailContent.js

--- frontend/src/components/Recipe/components/RecipeDetailContent/RecipeDetailContent.js
+++ frontend/src/components/Recipe/components/RecipeDetailContent/RecipeDetailContent.js
@@ -11,6 +11,7 @@ import {
 
 
 import {DarkButtonSmall, DivFlex} from "../../../../assets/styles/global";
+import iconClock from "../../../../assets/icons/icon_clock.png";
 
 
 import RecipeImages from "../RecipeImages/RecipeImages";
